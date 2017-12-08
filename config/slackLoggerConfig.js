13e096730d7e6edb28fcc04159dfc818c7bfa0fa
bill bittner <bittner.w@gmail.com>
2017-12-08 10:01:08 -0800

    updated slack config file

--- config/slackLoggerConfig.js
+++ config/slackLoggerConfig.js
@@ -1,34 +1,32 @@
-const SLACK_WEB_HOOK = process.env.SLACK_WEB_HOOK || 'none';
-let SLACK_ERROR_CHANNEL;
-let SLACK_INFO_CHANNEL;
-if (process.env.NODE_ENV === 'production') {
-    SLACK_ERROR_CHANNEL = '#speech-errors';
-    SLACK_INFO_CHANNEL = '#speech-logs';
-} else {
-    SLACK_ERROR_CHANNEL = '#staging_speech-errors';
-    SLACK_INFO_CHANNEL = '#staging_speech-logs';
-}
+const SLACK_WEB_HOOK =  'https://hooks.slack.com/services/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';
+const SLACK_ERROR_CHANNEL = '#dev_speech-errors';
+const SLACK_INFO_CHANNEL = null;
 
 const winstonSlackWebHook = require('winston-slack-webhook').SlackWebHook;
 
 module.exports = (winston) => {
   // add a transport for errors
-  winston.add(winstonSlackWebHook, {
-    name      : 'slack-errors-transport',
-    level     : 'error',
-    webhookUrl: SLACK_WEB_HOOK,
-    channel   : SLACK_ERROR_CHANNEL,
-    username  : 'speech_sync',
-    iconEmoji : ':face_with_head_bandage:',
-  });
-  winston.add(winstonSlackWebHook, {
-    name      : 'slack-info-transport',
-    level     : 'info',
-    webhookUrl: SLACK_WEB_HOOK,
-    channel   : SLACK_INFO_CHANNEL,
-    username  : 'speech_sync',
-    iconEmoji : ':nerd_face:',
-  });
+  if (SLACK_ERROR_CHANNEL) {
+    winston.add(winstonSlackWebHook, {
+      name      : 'slack-errors-transport',
+      level     : 'error',
+      webhookUrl: SLACK_WEB_HOOK,
+      channel   : SLACK_ERROR_CHANNEL,
+      username  : 'speech_sync',
+      iconEmoji : ':face_with_head_bandage:',
+    });
+  }
+  if (SLACK_INFO_CHANNEL) {
+    winston.add(winstonSlackWebHook, {
+      name      : 'slack-info-transport',
+      level     : 'info',
+      webhookUrl: SLACK_WEB_HOOK,
+      channel   : SLACK_INFO_CHANNEL,
+      username  : 'speech_sync',
+      iconEmoji : ':nerd_face:',
+    });
+  }
   // send test message
-  winston.error('Testing slack logging... slack logging is online.');
+  winston.error('Testing slack error logging... slack logging is online.');
+  winston.info('Testing slack info logging... slack logging is online.');
 };
